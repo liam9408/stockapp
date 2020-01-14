42db08944e91af8b916fc74b6d20369f39cacef1
liam9408 <57433615+liam9408@users.noreply.github.com>
2020-01-14 16:27:46 +0800

    added comments to api request file

--- API-request-method1/index.js
+++ API-request-method1/index.js
@@ -6,19 +6,32 @@ var symbol = 'AMZN'
 
 function getStock(){
     let http = new XMLHttpRequest();
+
+    // Calling the API
     http.open('GET', `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=5min&apikey=${apiKey}`);
 
-http.onreadystatechange = function () {
-    if (http.readyState != XMLHttpRequest.DONE) {
-        return;
-    } else if (http.status == 200) {
-       var data= JSON.parse((http.responseText))
-       console.log(data);
-    } else {
-        console.log('error occurred' + http.status);
+    http.onreadystatechange = function () {
+        if (http.readyState !== XMLHttpRequest.DONE) {
+            return;
+        } 
+        else if (http.status === 200) {
+        var data = JSON.parse((http.responseText))
+
+        // This will give you metadata about the stock, for example Stock Symbol, Time Zone...
+        console.log(data['Meta Data'])
+
+        // This will give you Stock Symbol
+        console.log(data['Meta Data']['2. Symbol'])
+
+        // // This will give you Stock Symbol, Time Zone etc
+        console.log(data['Time Series (5min)']);
+
+        } 
+        else {
+            console.log('error occurred' + http.status);
+        }
     }
-}
-http.send();
+    http.send();
 }
 
 getStock()
